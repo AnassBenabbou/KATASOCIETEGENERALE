ba3928a856c68d24cb16fb736eae10bae4b746c8
AnassBenabbou <49560090+AnassBenabbou@users.noreply.github.com>
2022-09-12 15:44:36 +0200

    Add files via upload

---
+++ NUnitTestProject1/NUnitTestProject1/Kata.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NUnitTestProject1
+{
+    public class Kata
+    {
+        public int CalculDigitZero(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == '|' && Tab2[1] == ' ' && Tab2[2] == '|' &&
+                Tab3[0] == '|' && Tab3[1] == '_' && Tab3[2] == '|')
+            {
+                return 0;
+            }
+            return -1;
+        }
+        public int CalculDigitUn(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == ' ' && Tab1[2] == ' ' &&
+                Tab2[0] == ' ' && Tab2[1] == ' ' && Tab2[2] == '|' &&
+                Tab3[0] == ' ' && Tab3[1] == ' ' && Tab3[2] == '|')
+            {
+                return 1;
+            }
+            return -1;
+        }
+        public int CalculDigitDeux(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == ' ' && Tab2[1] == '_' && Tab2[2] == '|' &&
+                Tab3[0] == '|' && Tab3[1] == '_' && Tab3[2] == ' ')
+            {
+                return 2;
+            }
+            return -1;
+        }
+        public int CalculDigitTrois(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == ' ' && Tab2[1] == '_' && Tab2[2] == '|' &&
+                Tab3[0] == ' ' && Tab3[1] == '_' && Tab3[2] == '|')
+            {
+                return 3;
+            }
+            return -1;
+        }
+        public int CalculDigitQuatre(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == ' ' && Tab1[2] == ' ' &&
+                Tab2[0] == '|' && Tab2[1] == '_' && Tab2[2] == '|' &&
+                Tab3[0] == ' ' && Tab3[1] == ' ' && Tab3[2] == '|')
+            {
+                return 4;
+            }
+            return -1;
+        }
+        public int CalculDigitCinq(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == '|' && Tab2[1] == '_' && Tab2[2] == ' ' &&
+                Tab3[0] == ' ' && Tab3[1] == '_' && Tab3[2] == '|')
+            {
+                return 5;
+            }
+            return -1;
+        }
+        public int CalculDigitSix(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == '|' && Tab2[1] == '_' && Tab2[2] == ' ' &&
+                Tab3[0] == '|' && Tab3[1] == '_' && Tab3[2] == '|')
+            {
+                return 6;
+            }
+            return -1;
+        }
+        public int CalculDigitSept(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == ' ' && Tab2[1] == ' ' && Tab2[2] == '|' &&
+                Tab3[0] == ' ' && Tab3[1] == ' ' && Tab3[2] == '|')
+            {
+                return 7;
+            }
+            return -1;
+        }
+        public int CalculDigitHuit(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == '|' && Tab2[1] == '_' && Tab2[2] == '|' &&
+                Tab3[0] == '|' && Tab3[1] == '_' && Tab3[2] == '|')
+            {
+                return 8;
+            }
+            return -1;
+        }
+        public int CalculDigitNeuf(List<char> Tab1, List<char> Tab2, List<char> Tab3)
+        {
+            if (Tab1[0] == ' ' && Tab1[1] == '_' && Tab1[2] == ' ' &&
+                Tab2[0] == '|' && Tab2[1] == '_' && Tab2[2] == '|' &&
+                Tab3[0] == ' ' && Tab3[1] == '_' && Tab3[2] == '|')
+            {
+                return 9;
+            }
+            return -1;
+        }
+
+        public string DigitEgal(List<char> tab1, List<char> tab2, List<char> tab3)
+        {
+            string digit = string.Empty;
+
+            if (CalculDigitZero(tab1, tab2, tab3) != -1)
+                return CalculDigitZero(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitUn(tab1, tab2, tab3) != -1)
+                return CalculDigitUn(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitDeux(tab1, tab2, tab3) != -1)
+                return CalculDigitDeux(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitTrois(tab1, tab2, tab3) != -1)
+                return CalculDigitTrois(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitQuatre(tab1, tab2, tab3) != -1)
+                return CalculDigitQuatre(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitCinq(tab1, tab2, tab3) != -1)
+                return CalculDigitCinq(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitSix(tab1, tab2, tab3) != -1)
+                return CalculDigitSix(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitSept(tab1, tab2, tab3) != -1)
+                return CalculDigitSept(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitHuit(tab1, tab2, tab3) != -1)
+                return CalculDigitHuit(tab1, tab2, tab3).ToString();
+
+            if (CalculDigitNeuf(tab1, tab2, tab3) != -1)
+                return CalculDigitNeuf(tab1, tab2, tab3).ToString();
+
+            return digit;
+        }
+
+        public string GenerateNumCompte(string ligne1, string ligne2, string ligne3)
+        {
+            List<char> List1 = ligne1.ToList();
+            List<char> List2 = ligne2.ToList();
+            List<char> List3 = ligne3.ToList();
+            string NumCompte = string.Empty;
+
+            int i = 0;
+            while (i <= List1.LongCount() - 4 && i <= List2.LongCount() - 4 && i <= List3.LongCount() - 4)
+            {
+                NumCompte += DigitEgal(new List<char> { List1[i], List1[i + 1], List1[i + 2] },
+                                       new List<char> { List2[i], List2[i + 1], List2[i + 2] },
+                                       new List<char> { List3[i], List3[i + 1], List3[i + 2] });
+                i = i + 4;
+            }
+
+            return NumCompte;
+        }
+    }
+}
---
+++ NUnitTestProject1/NUnitTestProject1/KataTest.cs
@@ -0,0 +1,216 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NUnitTestProject1
+{
+    public class KataTest
+    {
+        [Test]
+        public void DigitEgalZero()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { '|', ' ', '|' };
+            List<char> Tab3 = new List<char>() { '|', '_', '|' };
+            Assert.AreEqual(0, new Kata().CalculDigitZero(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalUn()
+        {
+            List<char> Tab1 = new List<char>() { ' ', ' ', ' ' };
+            List<char> Tab2 = new List<char>() { ' ', ' ', '|' };
+            List<char> Tab3 = new List<char>() { ' ', ' ', '|' };
+            Assert.AreEqual(1, new Kata().CalculDigitUn(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalDeux()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { ' ', '_', '|' };
+            List<char> Tab3 = new List<char>() { '|', '_', ' ' };
+            Assert.AreEqual(2, new Kata().CalculDigitDeux(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalTrois()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { ' ', '_', '|' };
+            List<char> Tab3 = new List<char>() { ' ', '_', '|' };
+            Assert.AreEqual(3, new Kata().CalculDigitTrois(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalQuatre()
+        {
+            List<char> Tab1 = new List<char>() { ' ', ' ', ' ' };
+            List<char> Tab2 = new List<char>() { '|', '_', '|' };
+            List<char> Tab3 = new List<char>() { ' ', ' ', '|' };
+            Assert.AreEqual(4, new Kata().CalculDigitQuatre(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalCinq()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { '|', '_', ' ' };
+            List<char> Tab3 = new List<char>() { ' ', '_', '|' };
+            Assert.AreEqual(5, new Kata().CalculDigitCinq(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalSix()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { '|', '_', ' ' };
+            List<char> Tab3 = new List<char>() { '|', '_', '|' };
+            Assert.AreEqual(6, new Kata().CalculDigitSix(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalSept()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { ' ', ' ', '|' };
+            List<char> Tab3 = new List<char>() { ' ', ' ', '|' };
+            Assert.AreEqual(7, new Kata().CalculDigitSept(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalHuit()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { '|', '_', '|' };
+            List<char> Tab3 = new List<char>() { '|', '_', '|' };
+            Assert.AreEqual(8, new Kata().CalculDigitHuit(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void DigitEgalNeuf()
+        {
+            List<char> Tab1 = new List<char>() { ' ', '_', ' ' };
+            List<char> Tab2 = new List<char>() { '|', '_', '|' };
+            List<char> Tab3 = new List<char>() { ' ', '_', '|' };
+            Assert.AreEqual(9, new Kata().CalculDigitNeuf(Tab1, Tab2, Tab3));
+
+        }
+
+        [Test]
+        public void NumCompteEgalZeros()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = "| | | | | | | | | | | | | | | | | | ";
+            string Ligne3 = "|_| |_| |_| |_| |_| |_| |_| |_| |_| ";
+
+            Assert.AreEqual("000000000", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalUns()
+        {
+            string Ligne1 = "                                    ";
+            string Ligne2 = "  |   |   |   |   |   |   |   |   | ";
+            string Ligne3 = "  |   |   |   |   |   |   |   |   | ";
+   
+            Assert.AreEqual("111111111", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalDeux()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = " _|  _|  _|  _|  _|  _|  _|  _|  _| ";
+            string Ligne3 = "|_  |_  |_  |_  |_  |_  |_  |_  |_  ";
+
+            Assert.AreEqual("222222222", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalTrois()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = " _|  _|  _|  _|  _|  _|  _|  _|  _| ";
+            string Ligne3 = " _|  _|  _|  _|  _|  _|  _|  _|  _| ";
+
+            Assert.AreEqual("333333333", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalQuatres()
+        {
+            string Ligne1 = "                                    ";
+            string Ligne2 = "|_| |_| |_| |_| |_| |_| |_| |_| |_| ";
+            string Ligne3 = "  |   |   |   |   |   |   |   |   | ";
+
+            Assert.AreEqual("444444444", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalCinqs()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = "|_  |_  |_  |_  |_  |_  |_  |_  |_  ";
+            string Ligne3 = " _|  _|  _|  _|  _|  _|  _|  _|  _| ";
+
+            Assert.AreEqual("555555555", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalSix()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = "|_  |_  |_  |_  |_  |_  |_  |_  |_  ";
+            string Ligne3 = "|_| |_| |_| |_| |_| |_| |_| |_| |_| ";
+
+            Assert.AreEqual("666666666", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+        [Test]
+        public void NumCompteEgalSepts()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = "  |   |   |   |   |   |   |   |   | ";
+            string Ligne3 = "  |   |   |   |   |   |   |   |   | ";
+
+            Assert.AreEqual("777777777", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+        [Test]
+        public void NumCompteEgalHuites()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = "|_| |_| |_| |_| |_| |_| |_| |_| |_| ";
+            string Ligne3 = "|_| |_| |_| |_| |_| |_| |_| |_| |_| ";
+
+            Assert.AreEqual("888888888", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalNeufs()
+        {
+            string Ligne1 = " _   _   _   _   _   _   _   _   _  ";
+            string Ligne2 = "|_| |_| |_| |_| |_| |_| |_| |_| |_| ";
+            string Ligne3 = " _|  _|  _|  _|  _|  _|  _|  _|  _| ";
+
+            Assert.AreEqual("999999999", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+
+        [Test]
+        public void NumCompteEgalMixte()
+        {
+            string Ligne1 = "     _   _       _   _   _   _   _  ";
+            string Ligne2 = "  |  _|  _| |_| |_  |_    | |_| |_| ";
+            string Ligne3 = "  | |_   _|   |  _| |_|   | |_|  _| ";
+
+            Assert.AreEqual("123456789", new Kata().GenerateNumCompte(Ligne1, Ligne2, Ligne3));
+        }
+    }
+}
